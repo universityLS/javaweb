4385ec070017c12aeba14bf0b84264e44e0e12ad
zhuangmingnan <zhuangmingnan@qq.com>
2017-06-04 00:37:46 +0800

    将项目转为Maven管理第三方依赖，注意看一下开发备忘

--- SpringMyBatis/src/ssm/controller/LoadIndexMess.java
+++ SpringMyBatis/src/ssm/controller/LoadIndexMess.java
@@ -1,18 +1,15 @@
 package ssm.controller;
 
 import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import net.sf.json.JSONObject;
 import ssm.orm.ThingsMess;
 import ssm.orm.ThingsMessMapper;
-import ssm.orm.Userinfo;
-import ssm.orm.Userinfos;
 
 @Controller
 @Transactional
